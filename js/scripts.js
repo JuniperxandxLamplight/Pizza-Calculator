668ecf9ccf9eaaa25779e0272481ea061a1fe670
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-02-01 11:15:33 -0800

    add price calculation function and results display

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,52 @@
 function Order(){
   this.pizzas = [];
+  this.items = 0;
 };
 
 Order.prototype.addPizza = function(size, crust, sauce, cheeseType, cheeseAmmount, meats, veggies){
+  this.items += 1;
   var newPizza = new Pizza(size, crust, sauce, cheeseType, cheeseAmmount, meats, veggies);
+  newPizza.name = "Pizza " + this.items;
   this.pizzas.push(newPizza);
-}
+};
+
+Order.prototype.checkPrice = function(){
+  var price = 0;
+  this.pizzas.forEach(function(pizza){
+    if (pizza.size === "Small (12\")"){
+      price += 10;
+    } else if (pizza.size === "Medium (16\")"){
+      price += 15;
+    } else if (pizza.size === "Large (20\")"){
+      price += 20;
+    } else if (pizza.size === "Extra Large (25\")"){
+      price += 25;
+    }
+
+    if (pizza.cheeseAmmount === "Extra"){
+      price += 3;
+    }
+
+    for (i=0; i<pizza.meats.length; i++){
+      if (price >= 25){
+        price += 2;
+      } else {
+        price += 1.5;
+      }
+    };
+    for (i=0; i<pizza.veggies.length; i++){
+      if (price >= 25){
+        price += 1.5;
+      } else {
+        price += 1;
+      }
+    };
+  });
+  return price;
+};
 
 function Pizza(size, crust, sauce, cheeseType, cheeseAmmount, meats, veggies){
+  this.name = name;
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
@@ -38,6 +77,7 @@ $(function(){
       return this.value;
     });
     newOrder.addPizza(sizeInput, crustInput, sauceInput, cheeseTypeInput, cheeseAmmountInput, meatsInput, veggiesInput);
+    console.log(newOrder);
   });
 
   $(".pizzaSelector").submit(function(event){
@@ -55,6 +95,11 @@ $(function(){
     });
     newOrder.addPizza(sizeInput, crustInput, sauceInput, cheeseTypeInput, cheeseAmmountInput, meatsInput, veggiesInput);
 
+    newOrder.pizzas.forEach(function(pizza){
+      $("#pizzaResults").append("<li>" + pizza.name + "</li>");
+    });
+    var newOrderPrice = newOrder.checkPrice();
+    $("#priceResult").text("$" + newOrderPrice);
     $(".pizzaSelector").slideUp(1500);
     $(".result").delay(1400).fadeIn("slow");
   });
