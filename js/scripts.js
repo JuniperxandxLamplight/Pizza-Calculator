100194c9d7a0fd8ce71e856e125c2fa04cb70c9a
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-02-01 09:04:31 -0800

    add html form for pizza building

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Order(){
+  this.pizzas = [];
+}
+
+function Pizza(){
+  this.size;
+  this.crust;
+  this.sauce;
+  this.cheeseType;
+  this.cheeseAmmount;
+  this.meats = [];
+  this.veggies = [];
+}
